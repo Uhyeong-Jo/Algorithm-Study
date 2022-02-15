e2057051d8f87461d796822f7808c9d7efa89bba
Uhyeong-Jo <62306702+Uhyeong-Jo@users.noreply.github.com>
2022-02-15 20:50:13 +0900

    Add files via upload

---
+++ Algorithm/Insertion_Sort.cpp
@@ -0,0 +1,26 @@
+//»ğÀÔ Á¤·Ä
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i, j, temp;
+    int num[10] = {1, 5, 6, 7, 10, 2, 8, 4, 3, 9};
+
+    for(int i = 0; i < 9; i++){
+        j = i;
+        while(j >= 0 && num[j] > num[j + 1]){
+            temp = num[j];
+            num[j] = num[j + 1];
+            num[j + 1] = temp;
+            j--;
+            /*
+            if(j < 0)
+                break;
+            */
+        }
+    }
+    for(int i = 0; i < 10; i++){
+        cout << num[i] << ' ';
+    }
+}
