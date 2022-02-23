cb3a2605b63f1b3092484846a947a1ece1f435dc
UhyeongJo <uh9222959@gmail.com>
2022-02-23 23:59:55 +0900

    fixing

---
+++ Algorithm/Sort/Heap_Sort.cpp
@@ -0,0 +1,18 @@
+//Èü Á¤·Ä
+//O(N*logN)
+  
+#include <iostream>
+using namespace std;
+
+const int num = 9;
+int heap[9] = {7, 6, 5, 8, 3, 9, 1, 6};
+
+int main(){
+    for(int i = 1; i < num; i++){
+        int c = i;
+        do{
+            int root = (c - 1) / 2;
+            if
+        }
+    }
+}
